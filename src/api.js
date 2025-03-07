227e2dc83275f25df114e6308dc47d88dfcd8812
SRR23 <srrimon267@gmail.com>
2025-03-07 16:18:21 +0600

    added

--- src/api.js
+++ src/api.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://shaidur.pythonanywhere.com/api/json-data/";
+export const BASE_URL = "https://shaidur.pythonanywhere.com/api/trades/";
 // export const BASE_URL = "http://127.0.0.1:8000/api/json-data/";
